d19de4c788a1dd87a12fe2052b3be35f2db3b4ef
dpc <dapengcheng@indexvc.com>
2019-10-12 19:21:45 +0800

    initial commit

---
+++ src/main/java/dto/User.java
@@ -0,0 +1,69 @@
+package dto;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User implements UserDetails {
+    private static final long serialVersionUID = 5359333896422354364L;
+
+    private Integer id;
+    private String username;
+    private String password;
+    private String role;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public User(Integer id, String username, String password, String role) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        this.authorities = Collections.singletonList(new SimpleGrantedAuthority(role));
+    }
+
+    @Override
+    @JsonDeserialize(using = CustomAuthorityDeserializer.class)
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
