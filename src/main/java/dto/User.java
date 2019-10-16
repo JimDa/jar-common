6e10ed04fd75d02bfeed4a5dcb70400d54bda88f
JimDa <1085529137@qq.com>
2019-10-16 23:18:47 +0800

    校验+属性定义

--- src/main/java/dto/User.java
+++ src/main/java/dto/User.java
@@ -4,31 +4,42 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import java.util.Collection;
-import java.util.Collections;
+import java.util.Date;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class User implements UserDetails {
+public class User extends BaseSchema implements UserDetails {
     private static final long serialVersionUID = 5359333896422354364L;
 
     private Integer id;
+    @NotBlank
+    @Length(min = 2, max = 8, message = "用户名长度在2到8个字符范围内！")
     private String username;
+    @NotBlank
+    @Length(min = 6, max = 10, message = "请输入6到10位数字字母组合的密码！")
     private String password;
-    private String role;
+    @Email
+    private String email;
+    private String gender;
+    private Date birthDate;
     private Collection<? extends GrantedAuthority> authorities;
 
-    public User(Integer id, String username, String password, String role) {
+    private List<String> roles;
+
+    public User(Integer id, String username, String password, List<String> roles) {
         this.id = id;
         this.username = username;
         this.password = password;
-        this.role = role;
-        this.authorities = Collections.singletonList(new SimpleGrantedAuthority(role));
+        this.roles = roles;
     }
 
     @Override
